fd8c38148ad4ae081a20dbcef8b55515a277f60b
RaphaelScheffler <pcraphaelscheffler@gmail.com>
2022-11-13 20:52:37 -0300

    Create prova.py

---
+++ prova.py
@@ -0,0 +1,13 @@
+def data(dia,mes,ano):
+    listaMes = ['janeiro', 'fevereiro',
+            'março','abril','maio',
+            'junho','julho', 'agosto',
+            'setembro', 'outubro',
+            'novembro', 'dezembro']
+    return f'Você nasceu em,'
+
+dia=int(input('Dia:'))
+mes=int(input('Mês:'))
+ano=int(input('Ano:'))
+
+data(dia,mes,ano)
